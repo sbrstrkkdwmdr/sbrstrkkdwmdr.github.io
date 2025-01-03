4018ac05c9ccce28ec76a0764b364a6cbd52e854
SaberStrike <71379681+sbrstrkkdwmdr@users.noreply.github.com>
2025-01-04 01:35:18 +1100

    use text logo svg

--- page_index.js
+++ page_index.js
@@ -1,12 +1,14 @@
 function generate(){
     const ctn = document.getElementById('content');
     {
-        const h1 = document.createElement('h1');
-        h1.innerText = 'SaberStrike';
-        h1.id = 'mainTitle'
-        h1.classList.add('pageTitle');
-        h1.classList.add('mainObject');
-        ctn.append(h1);
+        // const h1 = document.createElement('h1');
+        // h1.innerText = 'SaberStrike';
+        const img = document.createElement('img')
+        img.src = 'img/logo_text.svg'
+        img.id = 'mainTitle'
+        // img.classList.add('pageTitle');
+        img.classList.add('mainObject');
+        ctn.append(img);
     }
     {
         const img = document.createElement('img');
