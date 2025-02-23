4e54076f98e2deb5b3a00c572348881de2ff5b00
SaberStrike <71379681+sbrstrkkdwmdr@users.noreply.github.com>
2025-02-23 13:19:29 +1100

    add search function to skin dump

--- skins/page_dump.js
+++ skins/page_dump.js
@@ -1,198 +1,238 @@
-const skins = [
+let skinsList = [
+    {
+        name: 'Quantum',
+        id: 'quantum',
+        url: 'https://mega.nz/file/NvxgFYpS#nmwQl93gPlWjuyXj39g7cceh-BBcAEEQt4VOV2zpKtw',
+        image: 'quantum.jpg',
+        modes: ['osu']
+    },
     {
         name: '404SkinNotFound (defreeyay ver)',
+        id: '404nf',
         url: 'https://www.reddit.com/r/OsuSkins/comments/f8yfao/404skinnotfound_defreeyay_ver_std/',
         image: '404skinnotfounddefreeyay.jpg',
         modes: ['osu']
     },
     {
         name: '[AK] Teemo v1.0',
+        id: 'akteemo',
         url: 'https://skins.osuck.net/skins/2340?v=0',
         image: 'https://skins.osuck.net/images/screenshots/ad8914ab44303c84decb35bbaa2cda79.webp',
         modes: ['osu']
     },
     {
         name: 'Mrekk 3.0 - Aristia',
+        id: 'mrekk3aristia',
         url: 'https://drive.google.com/file/d/1tAKVY6ZQslhOsHlGguqQonnWygK-uC0B/view',
         image: 'mrekk3aristia.jpg',
         modes: ['osu'],
     },
     {
         name: 'saiyku [awawa]',
+        id: 'saiykuawawa',
         url: 'https://drive.google.com/file/d/1zP6ZFDVl_EesQb2BkkDzZj1s0y_WIoeb/view?usp=drive_link',
         image: 'awawa.jpg',
         modes: ['osu']
     },
     {
         name: 'azerino (sbr edit)',
+        id: 'azerino',
         url: 'https://www.mediafire.com/file/in3qokoh4437r69/-__edit_azerino.osk/file',
         image: 'azerino.jpg',
         modes: ['osu']
     },
     {
         name: 'cinia',
+        id: 'cinia',
         url: 'https://skins.osuck.net/skins/1626?v=0',
         image: 'https://skins.osuck.net/images/screenshots/348315af131ed4f6330dfd2654393900_xs.webp',
         modes: ['osu', 'taiko', 'fruits', 'mania']
     },
     {
         name: 'itsamemarioo (goink skin)',
+        id: 'goink',
         url: 'https://skins.osuck.net/skins/246?v=0',
         image: 'https://skins.osuck.net/images/screenshots/0709d9f3593cf0797eea4f20df6c4747.webp',
         modes: ['osu']
     },
     {
         name: 'gradient spam twighlight (sbr edit)',
+        id: 'gradient',
         url: 'https://www.mediafire.com/file/g0ssjhba80w2vum/-_SBR_MIX06_%255BGradient_Spam_Twighlight%255D.osk/file',
         image: 'gradientspam.jpg',
         modes: ['osu']
     },
     {
         name: 'Hayase Yuuka (forum edit)',
+        id: 'hayaseforum',
         url: 'https://www.mediafire.com/file/g4j5zh0euusfydu/Hayase_Yuuka_%2528worst_hr_player_edit%2529.osk/file',
         image: 'hayaseyuukaforum.jpg',
         modes: ['osu']
     },
     {
         name: 'Incandescent Nabulae',
+        id: 'ekoroblue',
         url: 'https://skins.osuck.net/skins/3017?v=0',
         image: 'https://skins.osuck.net/images/screenshots/75eb0968c0cad960bea79534739c5596_xs.webp',
         modes: ['osu', 'taiko', 'fruits', 'mania']
     },
     {
         name: 'Incandescent Cherry',
+        id: 'ekoropink',
         url: 'https://skins.osuck.net/skins/3017?v=1',
         image: 'https://skins.osuck.net/images/screenshots/6ca21b22851a1797f4a370c07c9a2dd1_xs.webp',
         modes: ['osu', 'taiko', 'fruits', 'mania']
     },
     {
         name: 'Jace 6.25',
+        id: 'jace',
         url: 'https://skins.osuck.net/skins/914?v=0',
         image: 'https://skins.osuck.net/images/screenshots/080f2be53f66f31c90c5bc7d8cb9212c_xs.webp',
         modes: ['osu']
     },
     {
         name: 'kantoku skin ruru (hvick225)',
+        id: 'hvick',
         url: 'https://skins.osuck.net/skins/2720?v=0',
         image: 'https://skins.osuck.net/images/screenshots/0186c1f8dd6f530cb971eb025e2f6461_xs.webp',
         modes: ['osu']
     },
     {
         name: 'karthy 6.0',
+        id: 'karthy',
         url: 'https://skins.osuck.net/skins/1168?v=0',
         image: 'https://skins.osuck.net/images/screenshots/03146f07a5c9b9d888362f42fbba43ce_xs.webp',
         modes: ['osu']
     },
     {
         name: 'Konata (rave party)',
+        id: 'konatarp',
         url: 'https://osu.ppy.sh/community/forums/topics/45689',
         image: 'https://i.ppy.sh/b8816a5c4c770c84ec372465c07ef217aa710efb/687474703a2f2f693633312e70686f746f6275636b65742e636f6d2f616c62756d732f757533372f566578745f486578742f4b6f6e617461253230536b696e2f6b733030332e706e67',
         modes: ['osu']
     },
     {
         name: 'lifeline v2 DT',
+        id: 'lifeline2',
         url: 'https://skins.osuck.net/skins/1804?v=0',
         image: 'https://skins.osuck.net/images/screenshots/afbc64f20bff142dece1c70d44260498_xs.webp',
         modes: ['osu']
     },
     {
         name: 'nezunyan pink (sbr edit)',
+        id: 'nezunyan',
         url: 'https://www.mediafire.com/file/30hv6zcq6wle5wd/-__edit_mod.osk/file',
         image: 'nezunyan-ymod.jpg',
         modes: ['osu']
     },
     {
         name: 'moeyandere umeniko',
+        id: 'umeniko',
         url: 'https://skins.osuck.net/skins/1465?v=0',
         image: 'https://skins.osuck.net/images/screenshots/aea140cd22625aafc25df02fd3aabb6b_xs.webp',
         modes: ['osu']
     },
     {
         name: 'monkoedit',
+        id: 'monko',
         url: 'https://skins.osuck.net/skins/2011?v=0',
         image: 'https://skins.osuck.net/images/screenshots/ab358180380acd6b11f7def15b844b20_xs.webp',
         modes: ['osu']
     },
     {
         name: 're;owotuna ck (sbr edit)',
+        id: 'retunack',
         url: 'https://www.mediafire.com/file/35ygdoa15es8pd7/-_%2540_re%253BsbrTuna_v1.1.1_%25E3%2580%258ECK%252Bedit%25E3%2580%258F_%2523_-.osk/file',
         image: 'resbrtunack.jpg',
         modes: ['osu']
     },
     {
         name: 're;owotuna',
+        id: 'retuna',
         url: 'https://skins.osuck.net/skins/1552?v=0',
         image: 'https://skins.osuck.net/images/screenshots/ef46fc899c7c5e63a5a53e3799561fd3_xs.webp',
         modes: ['osu']
     },
     {
         name: 'komori pegulian II',
+        id: 'pegulian2',
         url: 'https://skins.osuck.net/skins/1194?v=0',
         image: 'https://skins.osuck.net/images/screenshots/123573a5e83e3c780000720e016f7ea6_xs.webp',
         modes: ['osu']
     },
     {
         name: 'Rafis 2018-03-26 HDDT',
+        id: 'rafis',
         url: 'https://www.mediafire.com/file/phcsum9n4np44wh/Rafis_2018-03-26_HDDT.osk/file',
         image: 'rafis2018hddt.jpg',
         modes: ['osu']
     },
     {
         name: 'hello im rafis [hddt 2.0]',
+        id: 'rafis2',
         url: 'https://skins.osuck.net/skins/2528?v=0',
         image: 'https://skins.osuck.net/images/screenshots/82bf784cde469363754b80f97e8e35b4_xs.webp',
         modes: ['osu']
     },
     {
         name: 'rrtyuiskin',
+        id: 'rrtyui',
         url: 'https://skins.osuck.net/skins/347?v=0',
         image: 'https://skins.osuck.net/images/screenshots/f8719e5b4453965ef8e924e727441c1f_xs.webp',
         modes: ['osu', 'taiko', 'fruits', 'mania']
     },
     {
         name: 'shadowsoul 1.0',
+        id: 'shaddowsoul',
         url: 'https://www.mediafire.com/?okd9vcrfm1zcv34',
         image: 'shadowsoul.jpg',
         modes: ['osu']
     },
     {
         name: 'Seoul v9 Pineapple',
+        id: 'seoul',
         url: 'https://shigeskln.s-ul.eu/aZMjYmod', // https://gist.github.com/Fobxx/107e2bad2bf7312cd49431c696aac912#--seoul-v9-pineapple-color
         image: 'https://camo.githubusercontent.com/b7f7f5f606b2f11a6fc197e07c3d05fe9753ad88bc713ed90391e262a51ae952/68747470733a2f2f692e696d6775722e636f6d2f57704c79504d6e2e706e67',
         modes: ['osu']
     },
     {
         name: 'talala (sbr edit)',
+        id: 'talala',
         url: 'https://www.mediafire.com/file/zcy9ek7baso5qd2/-__edit_talala.osk/file',
         image: 'talala.jpg',
         modes: ['osu']
     },
     {
         name: 'Bacon boi',
+        id: 'bacon',
         url: 'https://skins.osuck.net/skins/1648?v=0',
         image: 'https://skins.osuck.net/images/screenshots/be60f582d0c541b13603d97cdfef40e6_xs.webp',
         modes: ['osu']
     },
     {
         name: 'whitecat 1.0',
+        id: 'whitecat',
         url: 'https://skins.osuck.net/skins/1107?v=0',
         image: 'https://skins.osuck.net/images/screenshots/c41d84e806e48a84b81fa790e82d3348_xs.webp',
         modes: ['osu']
     },
     {
         name: 'whitecurs',
+        id: 'whitecurs',
         url: 'https://www.mediafire.com/file/0zlb18gm6lv2x49/-__edit_whitecurs.osk/file',
         image: 'whitecurs.jpg',
         modes: ['osu']
     },
     {
         name: 'WWWskin',
+        id: 'www',
         url: 'https://skins.osuck.net/skins/327?v=0',
         image: 'https://skins.osuck.net/images/screenshots/30fc2c25408286deb6624b0ea5c15377.webp',
         modes: ['osu']
     },
     {
         name: 'YUGEN',
+        id: 'yugen',
         url: 'https://skins.osuck.net/skins/184?v=0',
         image: 'https://skins.osuck.net/images/screenshots/af672d6d91801b2974ed7bf8544ea3a2_xs.webp',
         modes: ['osu']
@@ -201,10 +241,28 @@ const skins = [
 ]
 
 function generate() {
-    const ctn = document.getElementById('content');
+    const ctn = document.getElementById('filterinput');
+    const textin = document.createElement('input');
+    textin.type = 'text';
+    textin.addEventListener('input', e => {
+        loadSkins(e.target.value ?? null);
+    });
+    textin.id = 'skinSearch';
+    textin.placeholder = "skin name"
+    ctn.append(textin);
+    loadSkins();
+}
 
+/**
+ * 
+ * @param {string} filter 
+ */
+function loadSkins(filter) {
+    const ctn = document.getElementById('skinshere');
+    ctn.innerHTML = ''
     const main = document.createElement('div');
     main.classList.add('itemGrid');
+    let skins = skinsList
     if (false) {
         skins.sort((a, b) => {
             if (a.name.toLowerCase() < b.name.toLowerCase()) {
@@ -216,10 +274,24 @@ function generate() {
             return 0;
         });
     }
+    if (filter && filter.length > 0) {
+        console.log(filter)
+        filter = filter.toLowerCase()
+        skins = skins.filter(x => {
+            let test = (x.id + '_' + x.name).toLowerCase()
+            if (test.includes(filter)) return true;
+            if (filter.includes(test)) return true;
+            if (filter == x.id) return true;
+            if (filter == x.name) return true;
+            if (x.modes.includes(filter.replace('std', 'osu'))) return true;
+        });
+        console.log(skins)
+    }
     skins.forEach(skin => {
         const a = document.createElement('a');
         a.href = skin.url;
         const item = document.createElement('section');
+        item.id = skin.id
         item.className = 'item';
         const h2 = document.createElement('h2');
         h2.className = 'itemName';
@@ -242,6 +314,5 @@ function generate() {
         a.append(item);
         main.append(a);
     })
-    ctn.append(main)
-
+    ctn.append(main);
 }
