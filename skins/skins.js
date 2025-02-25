288cdcce50a17a09c82cabd8be34717369b57a8d
SaberStrike <71379681+sbrstrkkdwmdr@users.noreply.github.com>
2025-02-26 00:12:47 +1100

    update v15

--- skins/skins.js
+++ skins/skins.js
@@ -2484,7 +2484,7 @@ default-n - based off <a class='highlightLink' href="https://fontmeme.com/fonts/
         date_end: 'present',
         description: 'non-gameplay still WIP',
         url: 'https://www.mediafire.com/folder/za6emwzaaawqo/15',
-        video_url: 'https://www.youtube.com/embed/v3tr9ilUfLQ',
+        video_url: 'https://www.youtube.com/embed/XCKW_Zg_qEI ',
         images: [
             // {
             //     name: 'Song select',
