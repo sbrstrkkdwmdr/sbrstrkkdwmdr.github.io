77a10674c0e632f5370fd0ec60c7dfb0632c7576
SaberStrike <71379681+sbrstrkkdwmdr@users.noreply.github.com>
2025-03-21 15:41:51 +1100

    main page links to other pages

--- page_index.js
+++ page_index.js
@@ -40,4 +40,62 @@ function generate() {
         })
         ctn.append(socialSection)
     }
+    {
+        const pages = [
+            {
+                name: 'osu! skins',
+                img: './skins/img/main_preview.jpg',
+                url: './skins',
+                keepRatio: false,
+                pixel: false,
+            },
+            {
+                name: 'Portfolio',
+                img: './img/icons/page.png',
+                url: './portfolio.html',
+                keepRatio: true,
+                pixel: true,
+            },
+            {
+                name: 'Other',
+                img: './img/icons/page.png',
+                url: './other.html',
+                keepRatio: true,
+                pixel: true,
+            },
+            {
+                name: 'Search',
+                img: './img/icons/search_dark.png',
+                url: './search.html',
+                keepRatio: true,
+                pixel: false,
+            }
+        ]
+        const pagesDiv = document.createElement('div');
+        pagesDiv.classList.add('itemGrid');
+        pages.forEach(page => {
+            const a = document.createElement('a');
+            a.href = page.url;
+            const item = document.createElement('section');
+            item.className = 'item';
+            const h2 = document.createElement('h2');
+            h2.className = 'itemName';
+            h2.innerText = page.name;
+            const img = document.createElement('img');
+            img.src = page.img;
+            img.className = "itemPreviewImage"
+            if (page.keepRatio) {
+                img.style.width = 'auto';
+            }
+            if (page.pixel) {
+                img.style.imageRendering = 'pixelated'
+            }
+            item.append(h2, img);
+            a.append(item);
+            pagesDiv.append(a);
+        })
+        const margin = document.createElement('div');
+        margin.style.marginTop = '250px';
+        ctn.append(margin, pagesDiv)
+    }
 }
